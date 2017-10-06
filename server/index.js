5273e3bbadf0adb3a7ab8e5d4b298d865b0e8e15
Nicolai <nicolaigaina@gmail.com>
2017-10-05 21:20:12 -0700

    changed greeting again.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'Hey, Kalimba, Suimon, Koster!!!' });
+    res.send({ hi: 'Kalimba, Suimon, Koster!!!' });
 });
 
 const PORT = process.env.PORT || 5000;
