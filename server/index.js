055c0e24be56a66ab550ecc6559b6eb906da1ea1
Nicolai <nicolaigaina@gmail.com>
2017-10-06 20:19:53 -0700

    Google OAuth Setup, Nodemon

--- server/index.js
+++ server/index.js
@@ -1,9 +1,35 @@
-const express = require('express'),
-    passport = require('passport'),
-    GoogleStrategy = require('passport-google-oauth20').Strategy,
-    app = express();
+const express = require("express"),
+  passport = require("passport"),
+  GoogleStrategy = require("passport-google-oauth20").Strategy,
+  keys = require("./config/keys");
+app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token " + accessToken);
+      console.log("refresh token " + refreshToken);
+      console.log("profile " + JSON.stringify(profile));
+    }
+  )
+);
+
+/* GOOGLE OAUTH */
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, "localhost", () => {
+  console.log("Dev server is running on port: " + PORT);
+});
