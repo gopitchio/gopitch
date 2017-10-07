887f4720d97c4beb0230162b0b276e4a91f91c66
Nicolai <nicolaigaina@gmail.com>
2017-10-06 19:20:40 -0700

    Passport, Google Strategy, Congig Keys.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
+const express = require('express'),
+    passport = require('passport'),
+    GoogleStrategy = require('passport-google-oauth20').Strategy,
+    app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'Kalimba, Suimon, Koster!!!' });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
