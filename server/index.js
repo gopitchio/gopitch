4e3623f26dcf5c2efe87ff989e504f58da159697
Nicolai <nicolaigaina@gmail.com>
2017-10-08 20:54:13 -0700

    user session, cookies, logout, current_user

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const keys = require("./config/keys");
+const express = require("express"),
+  mongoose = require("mongoose"),
+  cookieSession = require("cookie-session"),
+  passport = require("passport"),
+  keys = require("./config/keys");
+require("./models/user");
 require("./services/passport");
 
 /* CONNECT TO MONGODB */
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+/* USE COOKIES */
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+/* INIT PASSPORT FLOW */
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/auth")(app);
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,18 @@
 const passport = require("passport"),
   GoogleStrategy = require("passport-google-oauth20").Strategy,
-  keys = require("../config/keys");
+  keys = require("../config/keys"),
+  mongoose = require("mongoose");
+
+const UsersCollection = mongoose.model("users");
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+   UsersCollection.findById(id)
+   .then(user => done(null, user))
+   .catch(error => done(error));
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +22,19 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token " + accessToken);
-      console.log("refresh token " + refreshToken);
-      console.log("profile " + JSON.stringify(profile));
+      UsersCollection.findOne({ googleId: profile.id })
+        .then(existingUser => {
+          if (existingUser) {
+            done(null, existingUser);
+          } else {
+            const user = new UsersCollection({
+              googleId: profile.id
+            });
+
+            user.save().then(user => done(null, user));
+          }
+        })
+        .catch(error => done(error));
     }
   )
 );
